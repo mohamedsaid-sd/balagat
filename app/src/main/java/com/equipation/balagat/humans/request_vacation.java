0e39f7bccb0cde7ae1bfcebfb775652f6f5a3dbf
mohamedsaid-sd <sudanit2015@gmail.com>
2024-05-07 21:28:11 +0300

    7/5/2024 2screen human resourse update vication and prose

--- app/src/main/java/com/equipation/balagat/humans/request_vacation.java
+++ app/src/main/java/com/equipation/balagat/humans/request_vacation.java
@@ -10,21 +10,28 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import com.equipation.balagat.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 
 public class request_vacation extends AppCompatActivity {
 
-    LinearLayout layout_hag  ;
-    Spinner sp_vacation_type ;
-    ArrayList<String> ar_vication_type;
+    LinearLayout layout_hag , layout_relative  ;
+    Spinner sp_vacation_type , sp_relative ;
+    ArrayList<String> ar_vication_type , ar_relative ;
 
+    TextInputLayout ti_relative ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_vacation);
 
+
         ar_vication_type = new ArrayList<>();
+        ar_relative = new ArrayList<>();
+
+        ar_relative.add("نعم");
+        ar_relative.add("لا");
 
         ar_vication_type.add(" إجازة سنوية ");
         ar_vication_type.add(" إجازة مرضية ");
@@ -37,18 +44,52 @@ public class request_vacation extends AppCompatActivity {
 
         Casing();
 
+
+        layout_relative.setVisibility(View.GONE);
+        ti_relative.setVisibility(View.GONE);
+
         ArrayAdapter<String> adapter_spinner_borrow_type = new ArrayAdapter<>(this,
                 androidx.appcompat.R.layout.support_simple_spinner_dropdown_item , ar_vication_type);
         sp_vacation_type.setAdapter(adapter_spinner_borrow_type);
+
+        ArrayAdapter<String> adapter_relative = new ArrayAdapter<>(this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item , ar_relative);
+        sp_relative.setAdapter(adapter_relative);
+
 
         sp_vacation_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 if(ar_vication_type.get(i).equals("حج")){
+                    ti_relative.setVisibility(View.GONE);
                     layout_hag.setVisibility(View.VISIBLE);
+                    layout_relative.setVisibility(View.GONE);
+                }else if(ar_vication_type.get(i).equals("وفاة")){
+                    ti_relative.setVisibility(View.GONE);
+                    layout_hag.setVisibility(View.GONE);
+                    layout_relative.setVisibility(View.VISIBLE);
                 }else{
                     layout_hag.setVisibility(View.GONE);
+                    layout_relative.setVisibility(View.GONE);
+                    ti_relative.setVisibility(View.GONE);
 
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        sp_relative.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(ar_relative.get(i).equals("نعم")){
+                    ti_relative.setVisibility(View.VISIBLE);
+                }else{
+                    ti_relative.setVisibility(View.GONE);
                 }
             }
 
@@ -62,7 +103,10 @@ public class request_vacation extends AppCompatActivity {
 
     private void Casing() {
         sp_vacation_type = findViewById(R.id.sp_vacation_type);
+        sp_relative = findViewById(R.id.sp_relative);
         layout_hag= findViewById(R.id.layout_hag);
+        layout_relative = findViewById(R.id.layout_relative);
+        ti_relative = findViewById(R.id.ti_relative);
     }
 
     public void fun_back(View view) {
